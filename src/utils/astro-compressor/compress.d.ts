fe598472233e94f006f81b6e0f89e56d47c26985
Lars Vonk <larserikvonk@ziggo.nl>
2023-02-27 18:43:33 +0100

    removed unused code, improved astro-purge and audited

--- src/utils/astro-compressor/compress.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const gzip: (dir: string, enabled?: boolean) => Promise<void>;
-export declare const brotli: (dir: string, enabled?: boolean) => Promise<void>;
-//# sourceMappingURL=compress.d.ts.map
--- src/utils/astro-compressor/index.d.ts
+++
@@ -1,8 +0,0 @@
-import type { AstroIntegration } from "astro";
-interface Options {
-    gzip?: boolean;
-    brotli?: boolean;
-}
-export default function (opts?: Options): AstroIntegration;
-export {};
-//# sourceMappingURL=index.d.ts.map
